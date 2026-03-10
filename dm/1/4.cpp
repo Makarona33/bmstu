e403e502408f1e34de3619e441571cb46c2b162e
Makarona33 <140814600+Makarona33@users.noreply.github.com>
2026-03-10 23:38:08 +0300

    Update 4.cpp

--- dm/1/4.cpp
+++ dm/1/4.cpp
@@ -31,9 +31,6 @@ void dfs(int u, int parent) {
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
     cin >> N >> M;
     adj.resize(N);
     for (int i = 0; i < M; ++i) {
